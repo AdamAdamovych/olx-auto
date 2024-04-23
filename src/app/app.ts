a439bae7ec4653e70ecf1b9cfb65f0f4d13cdb46
Adam <adamkovuch@gmail.com>
2024-04-23 17:25:13 +0300

    added chrome logs

--- src/app/app.ts
+++ src/app/app.ts
@@ -13,25 +13,33 @@ export class App {
     private ads: Ads;
 
     constructor() {
-        this.browser = new AppBrowser();
-        this.appAuth = new AppAuth(this.browser);
-        this.copartAds = new CopartAds(this.browser);
-        this.ads = new Ads(this.browser, this.appAuth);
+        try {
+            this.browser = new AppBrowser();
+            this.appAuth = new AppAuth(this.browser);
+            this.copartAds = new CopartAds(this.browser);
+            this.ads = new Ads(this.browser, this.appAuth);
+        } catch(err) {
+            console.error('Error starting app. ', err);
+        }
     }
 
     async start() {
-        const copartUrl = prompt('Please enter Copart url >> ');
-        const copartData = await this.copartAds.getFrom(copartUrl);
+        try {
+            const copartUrl = prompt('Please enter Copart url >> ');
+            const copartData = await this.copartAds.getFrom(copartUrl);
 
-        console.log(copartData);
-        await this.ads.open();
+            console.log(copartData);
+            await this.ads.open();
 
-        /*const testData: Partial<CopartItem> = {
-            info: {title: '2018 Audi A4 B9 Premium'} as any,
-            images: ['D:\\Dev\\olx_auto\\tmp\\img.jpg', 'D:\\Dev\\olx_auto\\tmp\\img2.jpg']
-        }*/
-        await this.ads.setData(copartData);
+            /*const testData: Partial<CopartItem> = {
+                info: {title: '2018 Audi A4 B9 Premium'} as any,
+                images: ['D:\\Dev\\olx_auto\\tmp\\img.jpg', 'D:\\Dev\\olx_auto\\tmp\\img2.jpg']
+            }*/
+            await this.ads.setData(copartData);
 
-        //this.browser.close();
+            //this.browser.close();
+        } catch(err) {
+            console.error(err);
+        }
     }
 }
